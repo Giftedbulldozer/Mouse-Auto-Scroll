02f66da2791fa04baea6c58eb8d811f01ad3e914
GiftedBulldozere <GiftedBulldozer@gmail.com>
2020-10-12 10:54:35 -0500

    Add project files.

---
+++ Mouse Auto Scroll/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Mouse_Auto_Scroll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtScrollPerSecond = new System.Windows.Forms.TextBox();
+            this.lblScrollPerSecond = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.tmrMouseScrollTimer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmbxCurrentProcesses = new System.Windows.Forms.ComboBox();
+            this.lblProcesses = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtScrollPerSecond
+            // 
+            this.txtScrollPerSecond.Location = new System.Drawing.Point(12, 56);
+            this.txtScrollPerSecond.Name = "txtScrollPerSecond";
+            this.txtScrollPerSecond.Size = new System.Drawing.Size(80, 20);
+            this.txtScrollPerSecond.TabIndex = 0;
+            // 
+            // lblScrollPerSecond
+            // 
+            this.lblScrollPerSecond.AutoSize = true;
+            this.lblScrollPerSecond.Location = new System.Drawing.Point(12, 37);
+            this.lblScrollPerSecond.Name = "lblScrollPerSecond";
+            this.lblScrollPerSecond.Size = new System.Drawing.Size(92, 13);
+            this.lblScrollPerSecond.TabIndex = 1;
+            this.lblScrollPerSecond.Text = "Scroll Per Second";
+            this.toolTip1.SetToolTip(this.lblScrollPerSecond, "How fast do you want the mouse to scroll per second");
+            // 
+            // tmrMouseScrollTimer
+            // 
+            this.tmrMouseScrollTimer.Tick += new System.EventHandler(this.tmrMouseScrollTimer_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(247, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // cmbxCurrentProcesses
+            // 
+            this.cmbxCurrentProcesses.FormattingEnabled = true;
+            this.cmbxCurrentProcesses.Location = new System.Drawing.Point(12, 109);
+            this.cmbxCurrentProcesses.Name = "cmbxCurrentProcesses";
+            this.cmbxCurrentProcesses.Size = new System.Drawing.Size(223, 21);
+            this.cmbxCurrentProcesses.TabIndex = 3;
+            // 
+            // lblProcesses
+            // 
+            this.lblProcesses.AutoSize = true;
+            this.lblProcesses.Location = new System.Drawing.Point(12, 93);
+            this.lblProcesses.Name = "lblProcesses";
+            this.lblProcesses.Size = new System.Drawing.Size(93, 13);
+            this.lblProcesses.TabIndex = 4;
+            this.lblProcesses.Text = "Current Processes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 217);
+            this.Controls.Add(this.lblProcesses);
+            this.Controls.Add(this.cmbxCurrentProcesses);
+            this.Controls.Add(this.lblScrollPerSecond);
+            this.Controls.Add(this.txtScrollPerSecond);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Auto Mouse Scroller";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtScrollPerSecond;
+        private System.Windows.Forms.Label lblScrollPerSecond;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Timer tmrMouseScrollTimer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ComboBox cmbxCurrentProcesses;
+        private System.Windows.Forms.Label lblProcesses;
+    }
+}
+
---
+++ Mouse Auto Scroll/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mouse_Auto_Scroll
+{  
+    public partial class Form1 : Form
+    {
+        #region Dll Imports
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct KEYBDINPUT
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct HARDWAREINPUT
+        {
+            public uint uMsg;
+            public ushort wParamL;
+            public ushort wParamH;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct INPUT
+        {
+            [FieldOffset(0)]
+            public int type;
+            [FieldOffset(4)]
+            public MOUSEINPUT mi;
+            [FieldOffset(4)]
+            public KEYBDINPUT ki;
+            [FieldOffset(4)]
+            public HARDWAREINPUT hi;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);        
+        const uint MOUSEEVENTF_WHEEL = 0x0800;
+
+
+        private MOUSEINPUT createMouseInput(int x, int y, uint data, uint t, uint flag)
+        {
+            MOUSEINPUT mi = new MOUSEINPUT();
+            
+            mi.mouseData = data;
+            
+            mi.dwFlags =  MOUSEEVENTF_WHEEL;            
+            return mi;
+        }
+
+
+        #endregion
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        //...
+
+        Process p; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            if (txtScrollPerSecond.Text != String.Empty)
+            {
+                if (cmbxCurrentProcesses.Text != string.Empty)
+                {
+                    
+                    double enteredValue = double.Parse(txtScrollPerSecond.Text, System.Globalization.CultureInfo.InvariantCulture);
+                    TimeSpan ts = TimeSpan.FromSeconds(enteredValue);
+                    tmrMouseScrollTimer.Interval = (int)ts.TotalMilliseconds;
+                    if (tmrMouseScrollTimer.Enabled == false)
+                        tmrMouseScrollTimer.Start();
+                }
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        { if (e == null)
+                return;
+            else if(e.KeyCode == Keys.F8)
+            {
+                tmrMouseScrollTimer.Stop();
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            tmrMouseScrollTimer.Stop();
+        }
+
+        private bool FormNotActive()
+        {
+            if( Form1.ActiveForm != this)
+            return true;
+            else
+            return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> processList = new List<string>();
+            
+            foreach(Process proc in Process.GetProcesses())
+            {
+                processList.Add(proc.ProcessName);                
+            }
+
+            processList.Sort();
+            foreach(string process in processList)
+            {
+                cmbxCurrentProcesses.Items.Add(process);
+            }
+        }
+
+        private void tmrMouseScrollTimer_Tick(object sender, EventArgs e)
+        {
+            p = Process.GetProcessesByName(cmbxCurrentProcesses.Text).FirstOrDefault();
+            if (p.ProcessName == string.Empty)
+            {
+                tmrMouseScrollTimer.Stop();
+                MessageBox.Show("An error Occurred while getting the window specified");
+                return;
+            }
+            
+            INPUT[] inp = new INPUT[2];
+            inp[0].mi = createMouseInput(0, 0, 50, 0, MOUSEEVENTF_WHEEL);
+            SendInput((uint)inp.Length, inp, Marshal.SizeOf(inp[0].GetType()));
+            if (Control.ModifierKeys == Keys.F8)
+                tmrMouseScrollTimer.Stop();
+           
+        }
+    }
+}
